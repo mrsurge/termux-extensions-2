3e1c7abb1773c723187fcd36cfc9c4bc8d0de451
Sergio Perez <mrsergioperez@gmail.com>
2025-12-08 05:09:45 -0600

    fixed hierarchical

--- app/static/vendor/nicegui/elements/codemirror/codemirror.js
+++ app/static/vendor/nicegui/elements/codemirror/codemirror.js
@@ -748,9 +748,26 @@ export default {
       this.lspTransport = transport;
 
       try {
+        // Add extension to request hierarchical DocumentSymbol (with children) instead of flat SymbolInformation
+        // This is required for nested sticky scroll to work correctly
+        const hierarchicalSymbolCapability = {
+          clientCapabilities: {
+            textDocument: {
+              documentSymbol: {
+                hierarchicalDocumentSymbolSupport: true,
+                symbolKind: {
+                  valueSet: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+                },
+                labelSupport: true
+              }
+            }
+          }
+        };
+        
         this.lspClient = new LSPClient({
           rootUri: 'file://' + projectRoot,
           workspaceFolders: [{ name: 'root', uri: 'file://' + projectRoot }],
+          extensions: [hierarchicalSymbolCapability],
         });
       } catch (err) {
         console.error('[LSP] Failed to create LanguageServerClient:', err);
