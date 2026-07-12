0766d010f2a000a08ca719c42a112be50744bfc8
xrsurge <xrsurge@gmail.con>
2026-07-12 16:25:26 -0500

    Disable service workers in Android shells

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.termux.extensions"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20302
-        versionName = "1.0.7-r0.2.302"
+        versionCode = 20303
+        versionName = "1.0.7-r0.2.303"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["sharedUserIdValue"] = "com.termux.extensions.base"
